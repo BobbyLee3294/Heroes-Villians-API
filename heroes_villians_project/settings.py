08bedeaed4c04b029f28cfb77439e773bace390a
Bobby Lee <thayungbobby@gmail.com>
2022-11-14 14:08:07 -0500

    corrected directory (didnt type the period at the end of startproject command)

--- heroes_villians_project/settings.py
+++ heroes_villians_project/settings.py
@@ -105,3 +105,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from heroes_villians_project.local_settings import *
+
+except ImportError:
+    pass
