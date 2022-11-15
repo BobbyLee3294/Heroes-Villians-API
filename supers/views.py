9e37a0ae4ee85a952dd8a81f56546abf7e3bb84d
Bobby Lee <thayungbobby@gmail.com>
2022-11-15 15:10:31 -0500

    added get by id, post, and delete endpoints

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperSerializer
+from .models import Super
 
-# Create your views here.
+@api_view
+def supers_list(request):
+
+    pass
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def supers_detail(request, pk):
+
+    super = get_object_or_404(Super, pk=pk)
+
+    if request.method == 'GET':
+        serializer = SuperSerializer(super)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == 'POST':
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    if request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
