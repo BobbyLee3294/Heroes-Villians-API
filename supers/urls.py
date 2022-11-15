3014ac9d8553e331f8660c7e29fd92ea90e59519
Bobby Lee <thayungbobby@gmail.com>
2022-11-15 13:50:28 -0500

    Changes to be committed: 	modified:   supers/urls.py

--- supers/urls.py
+++ supers/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.supers_list), # views.supers_list, optional params
-    path('<int:pk>/', views.super_detail), # views.supers_details
-]
+    path('', views.supers_list),
+    path('<int:pk>/', views.super_detail),
